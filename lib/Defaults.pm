07129cd9a47c3c17146c3922af317b46dca9ddd9
Thomas B. Mooney <tmooney@genome.wustl.edu>
2021-03-08 12:41:34 -0600

    Remove unused defaults.

--- lib/Defaults.pm
+++ lib/Defaults.pm
@@ -1,9 +1,5 @@
 package Defaults;
 
-sub BSUB_EMAIL_TO { return 'apipe-tester@genome.wustl.edu'};
-
-sub GIT_BIN { return '/gsc/bin/git'};
-
 my $BASE_DIR = '/gsc/scripts/opt/genome';
 sub BASE_DIR { return $BASE_DIR };
 sub DIST_DIR { return $BASE_DIR . '/dist' };
@@ -15,17 +11,6 @@ sub CURRENT_PIPELINE { return $BASE_DIR . '/current/pipeline'};
 sub CURRENT_WEB { return $BASE_DIR . '/current/web'};
 sub CURRENT_USER { return $BASE_DIR . '/current/user'};
 
-sub UR_REPOSITORY { return 'git://github.com/sakoht/UR.git'};
-sub GENOME_REPOSITORY { return 'ssh://git.gsc.wustl.edu/srv/git/genome.git'};
-
 sub GIT_REPOS_BASE { return '/gscuser/apipe-tester/.hudson_repos'};
-sub REPOSITORIES {
-    return (
-    	UR => 'git://github.com/sakoht/UR.git',
-    	genome => 'ssh://git.gsc.wustl.edu/srv/git/genome.git',
-    );
-};
-
-sub HUDSON_CUSTOM_WORKSPACE { return '/gscuser/apipe-tester/.hudson-workspaces' };
 
 1;
